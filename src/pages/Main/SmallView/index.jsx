d0455e36ad09201d1bbc61c63b8b46b94a5d6198
Gustavo Santos <gustavoms97@hotmail.com>
2020-05-05 18:07:19 -0300

    Iniciada a visão em cards da listagem de usuários

---
+++ src/pages/Main/SmallView/index.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+import { Container } from "./style.css";
+
+class SmallView extends Component {
+  render() {
+    return (
+      <Container>
+        <h1>teste</h1>
+      </Container>
+    );
+  }
+}
+
+export default SmallView;
